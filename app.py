9707f4bba68968fef48ce7da8e05025516f1a01b
alpana11 <alpanapardeshi2706@gmail.com>
2026-04-13 23:30:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
 from ultralytics import YOLO
+import os
+
 
 app = Flask(__name__)
 
 API_KEY = "mysecret123"
 
-model = YOLO("best.pt")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "best.pt")
+
+model = YOLO(model_path)
 
 @app.route('/predict', methods=['POST'])
 def predict():
